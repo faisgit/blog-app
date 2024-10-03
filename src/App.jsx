e7f9807602dd07d16bec5d9d400d2fc465f14416
faisgit <fa830163@gmail.com>
2024-10-03 07:16:25 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import conf from './conf/conf'
+
+function App() {
+
+  console.log(conf.appwriteBucketId)
+  return (
+    <>
+     <h1>A Blog with AppWrite</h1>
+    </>
+  )
+}
+
+export default App
